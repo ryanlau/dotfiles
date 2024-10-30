2f0b404b486fd3302ded6e7c6aee78de4d3eb029
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2024-10-30 13:54:18 -0400

    init home-manager for non macos systems

---
+++ dot_config/home-manager/home.nix
@@ -0,0 +1,24 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "ryan";
+  home.homeDirectory = "/home/ryan";
+
+  home.stateVersion = "24.05";
+
+  home.packages = with pkgs; [
+		chezmoi
+		eza
+		fastfetch
+		fnm
+		fzf
+		go
+		lua
+		neovim
+		zoxide
+		zsh
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
